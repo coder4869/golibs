b2d062d441d9306f0e590724d640bde9638fd3fd
”coder4869“ <coder4869@gmail.com>
2016-08-23 14:57:19 +0800

    first submit glcrypto

---
+++ glcrypto/glAesDes.go
@@ -0,0 +1,145 @@
+//From:  https://github.com/polaris1119/myblog_article_code
+package glcrypto
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/des"
+)
+
+/******************** AES ********************/
+
+func AesEncrypt(origData, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockSize := block.BlockSize()
+	origData = PKCS5Padding(origData, blockSize)
+	//	origData = ZeroPadding(origData, block.BlockSize())
+	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
+	crypted := make([]byte, len(origData))
+	// according to the description of CryptBlocks() function,
+	// the init way "crypted := origData" also right
+	blockMode.CryptBlocks(crypted, origData)
+	return crypted, nil
+}
+
+func AesDecrypt(crypted, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
+	origData := make([]byte, len(crypted))
+	// origData := crypted
+	blockMode.CryptBlocks(origData, crypted)
+	origData = PKCS5UnPadding(origData)
+	//	origData = ZeroUnPadding(origData)
+	return origData, nil
+}
+
+/******************** DES ********************/
+
+//len(key) == 8
+func DesEncrypt(origData, key []byte) ([]byte, error) {
+
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	origData = PKCS5Padding(origData, block.BlockSize())
+	// origData = ZeroPadding(origData, block.BlockSize())
+	blockMode := cipher.NewCBCEncrypter(block, key)
+	crypted := make([]byte, len(origData))
+	// according to the description of CryptBlocks() function,
+	// the init way "crypted := origData" also right
+	blockMode.CryptBlocks(crypted, origData)
+
+	return crypted, nil
+}
+
+//len(key) == 8
+func DesDecrypt(crypted, key []byte) ([]byte, error) {
+
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	blockMode := cipher.NewCBCDecrypter(block, key)
+	origData := make([]byte, len(crypted))
+	// origData := crypted
+	blockMode.CryptBlocks(origData, crypted)
+	origData = PKCS5UnPadding(origData)
+	// origData = ZeroUnPadding(origData)
+
+	return origData, nil
+}
+
+/******************** 3DES ********************/
+
+// 3DES, len(key) == 24
+func TripleDesEncrypt(origData, key []byte) ([]byte, error) {
+
+	block, err := des.NewTripleDESCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	origData = PKCS5Padding(origData, block.BlockSize())
+	// origData = ZeroPadding(origData, block.BlockSize())
+	blockMode := cipher.NewCBCEncrypter(block, key[:8])
+	crypted := make([]byte, len(origData))
+	blockMode.CryptBlocks(crypted, origData)
+
+	return crypted, nil
+}
+
+// 3DES, len(key) == 24
+func TripleDesDecrypt(crypted, key []byte) ([]byte, error) {
+
+	block, err := des.NewTripleDESCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	blockMode := cipher.NewCBCDecrypter(block, key[:8])
+	origData := make([]byte, len(crypted))
+	// origData := crypted
+	blockMode.CryptBlocks(origData, crypted)
+	origData = PKCS5UnPadding(origData)
+	// origData = ZeroUnPadding(origData)
+
+	return origData, nil
+}
+
+/******************** Padding for AES and DES ********************/
+
+func ZeroPadding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{0}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func ZeroUnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
+
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	// remove last byte, unpadding times
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
---
+++ glcrypto/glBase64.go
@@ -0,0 +1,48 @@
+package glcrypto
+
+import (
+	"encoding/base64"
+
+	"github.com/coder4869/golibs/glio"
+)
+
+/**********************************************************/
+// AES-128 encrypt, then base64 encode. key length：16, 24, 32 bytes to AES-128, AES-192, AES-256
+func Base64AesEnResult(orig []byte, key []byte) (string, error) {
+	glio.FLPrintf("Before Aes Encrypt: %v\n", string(orig))
+	aesresult, err := AesEncrypt([]byte(orig), key)
+	if err != nil {
+		//	panic(err)
+		return "null", err
+	}
+	respDes := base64.StdEncoding.EncodeToString(aesresult)
+	return respDes, nil
+}
+
+// base64 decode, then AES-128 decrypt. key length：16, 24, 32 bytes to AES-128, AES-192, AES-256
+func Base64AesDeResult(crypted string, key []byte) (string, error) {
+	glio.FLPrintf("Before Aes Decrypt: " + crypted)
+	origAes, err := base64.StdEncoding.DecodeString(crypted)
+	if err != nil {
+		return "null", err
+	}
+
+	origStr, err := AesDecrypt(origAes, key)
+	if err != nil {
+		return "null", err
+	}
+
+	return string(origStr), nil
+}
+
+// base64 encoding result of DES
+func Base64EncodeDesResult(orig []byte, key []byte) (string, error) {
+	glio.FLPrintf("Before Des: " + string(orig))
+	desresult, err := DesEncrypt([]byte(orig), key)
+	if err != nil {
+		return "null", err
+	}
+	respDes := base64.StdEncoding.EncodeToString(desresult)
+	glio.FLPrintf(respDes)
+	return respDes, nil
+}
